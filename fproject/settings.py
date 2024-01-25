b76f695f5826b4ae6b4e4d3b828169fef8f29f33
robinabhishek06 <robinabhishek1@gmail.com>
2024-01-25 16:56:57 +0530

    asfsd

--- fproject/settings.py
+++ fproject/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +25,7 @@ SECRET_KEY = 'django-insecure-1yy$jlbmf!#pko9l-jr-#rw=p%_v86#xide93zwbd3hlgca$a0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -117,17 +118,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'authentication' / 'static',
-]
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'authentication' / 'static',
+# ]
+# STATIC_ROOT = BASE_DIR /'staticfiles'/'static_root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR /'staticfiles'/'static_root'
 
-# import os
-# STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
-# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+STATICFILES_DIRS=os.path.join(BASE_DIR,'authentication/static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','authentication/static')
